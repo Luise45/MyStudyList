26da318ccc2538a5f7e22f4f0bfd58ad2d0e8147
Luise <luisetabatt@gmail.com>
2026-06-05 12:19:13 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,26 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+
+
+
+
+app.use(cors({
+  origin: 'https://frontend-o8f1tfmsz-luise-tabatts-projects.vercel.app'
+}));
 app.use(express.json());
 
 
 app.use('/api/hws', hwRoutes);
-
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 })
-.catch(err => console.error(err));
