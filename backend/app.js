0ec41ae50562acf8a4985797a3d15e72d4cb572b
Luise45 <Luise.Tabatt@Student.HTW-Berlin.de>
2026-09-01 09:42:29 +0200

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,6 @@ app.use(express.json());
 
 app.use('/api/hws', hwRoutes);
 const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
