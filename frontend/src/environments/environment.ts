d1619bee201778dcb85473f7116ced3eb2b9bcc2
Luise45 <Luise.Tabatt@Student.HTW-Berlin.de>
2026-09-02 11:27:22 +0200

    aoi

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,4 @@
-const production = false; // toggle this (or use a build-time flag)
-
 export const environment = {
-  production,
-  apiUrl: production
-    ? 'https://mystudylist-backend-612429176168.europe-west1.run.app'
-    : 'http://localhost:5000'
+  production: true,
+  apiUrl: 'https://mystudylist-backend-612429176168.europe-west1.run.app'
 };
