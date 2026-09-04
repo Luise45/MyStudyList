61688eb416c6517f05f30c17a5141283dda1ff6e
Luise45 <Luise.Tabatt@Student.HTW-Berlin.de>
2026-09-04 20:59:27 +0200

    start of using a test database

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,9 @@ mongoose.connect(process.env.MONGO_URI)
       console.log(`Server running on port ${PORT}`);
     });
   })
+
+
+  
   .catch((error) => {
     console.error('MongoDB connection failed:', error);
     process.exit(1);
