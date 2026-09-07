36d1ffb1b2382baaf583e3218e94460b9fea58ab
Luise45 <Luise.Tabatt@Student.HTW-Berlin.de>
2026-09-07 13:54:35 +0200

    changed from chrome to edge

--- playwright.config.js
+++ playwright.config.js
@@ -43,11 +43,16 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome']
+     name: 'chromium',
+  use: {
+    ...devices['Desktop Edge'],
+    channel: 'msedge',
+  },
       },
   
-    },
+    
+
+    
 
    // {
    //   name: 'firefox',
