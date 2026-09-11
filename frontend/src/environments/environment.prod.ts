24b8ad4d0896636f13ef48ddf453fca2cdceb909
Luise45 <Luise.Tabatt@Student.HTW-Berlin.de>
2026-09-11 17:39:03 +0200

    fix in evn url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-
 export const environment = {
   production: true,
-  apiUrl: 'https://mystudylist-backend-612429176168.europe-west1.run.app'
+  apiUrl: 'https://mystudylist-backend-612429176168.europe-west1.run.app/api/auth'
 };
